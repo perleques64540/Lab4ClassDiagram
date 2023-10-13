2244a4fa6de30d40b22dca6202de39b015d2f8c2
perleques64540 <115733744+perleques64540@users.noreply.github.com>
2023-10-13 13:17:20 +0100

    First commit

---
+++ Model/Emplyee.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Emplyee extends UniversityMember {
+
+    /**
+     * Default constructor
+     */
+    public Emplyee() {
+    }
+
+    /**
+     * 
+     */
+    private int acctNo;
+
+    /**
+     * @return
+     */
+    public int getAccNo() {
+        // TODO implement here
+        return 0;
+    }
+
+}
